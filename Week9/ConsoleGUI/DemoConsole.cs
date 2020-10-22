b10931641d5b972b425bc7578b639b85db5754b3
Chad Zuniga <chad.zuniga10@gmail.com>
2020-10-21 21:05:56 -0600

    week 9 day 2

---
+++ Week9/ConsoleGUI/DemoConsole.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleGUI
+{
+    public class DemoConsole
+    {
+
+        public static void DemoColor()
+        {
+            Console.WriteLine("T I T L E");
+            Console.WriteLine();
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            var height = 19;
+            var topOfSq = 4;
+            var margin = 3;
+
+            for (var n = 0; n < height / 2; n++)
+            {
+                Console.CursorTop = topOfSq + n;
+                toggleColor();
+                for (var i = 0; i < height - n*2; i++)
+                {
+                    var str = "{0," + (2 * height - (n*4)) + " }";
+                    Console.CursorLeft = margin + (n*2);
+                    Console.WriteLine(str, " ");
+                }
+
+            }
+
+            Console.CursorTop = 30;
+        }
+
+        private static void toggleColor()
+        {
+            if (Console.BackgroundColor == ConsoleColor.DarkGreen)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkMagenta;
+
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.DarkGreen;
+            }
+        }
+    }
+}
---
+++ Week9/ConsoleGUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleGUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DemoConsole.DemoColor();
+
+            
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("\n\n");
+        }
+    }
+}
+
+    
